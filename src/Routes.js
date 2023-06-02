5417b92d00a193aae5b3f478e77ce4a9e8e623d0
Trex96 <jarvis7297@gmail.com>
2023-06-02 20:10:09 +0530

    Error page,OTP

--- src/Routes.js
+++ src/Routes.js
@@ -3,15 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import DashBoard from "./pages/DashBoard";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+import ErrorPage from "./pages/Error";
 import SignUp from "./pages/SignUp";
 import About from "./pages/AboutUs";
 import Teams from "./pages/Teams";
 import Services from "./pages/Services";
+import Subscription from "./pages/Subscription";
 import Query from "./pages/Query";
 import ForgotPassword from "./pages/ForgotPassword";
 import PaymentFailed from "./pages/PaymentFailed";
 import PaymentSuccessful from "./pages/PaymentSuccessful";
-import Logout from "./pages/Logout";
 
 const RoutesContainer = () => {
   return (
@@ -24,10 +26,12 @@ const RoutesContainer = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/logout" element={<Logout />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/subscription" element={<Subscription />} />
       <Route path="/query" element={<Query />} />
       <Route path="/forgotpassword" element={<ForgotPassword />} />
       <Route path="/paymentsuccessful" element={<PaymentSuccessful />} />
       <Route path="/paymentfailed" element={<PaymentFailed />} />
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
 };
---
+++ src/pages/Subscription.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import "../Components/comp/Subscription.css";
+import Product from "../Components/Subscription/Product";
+// import BworkzInstructorMonthly from "../Subscription/BworkzInstructorMonthly";
+// import BworkzInstructorYearly from "../Subscription/BworkzInstructorYearly";
+import Context from "../Context/Context";
+import NavBar from "../Components/NavBar";
+export default function Subscription() {
+  const Ctx = useContext(Context);
+
+  return (
+    <>
+    <NavBar />
+      <section className="Back  text-[1.5rem]  flex  flex-col items-center h-[50rem] max1414:h-[auto] justify-center gap-[5rem] pb-20 ">
+        <div className="text-center text-[white] text-[3.4rem] mt-20 font-bebas-neue ">
+          <h1>check out our exclusive offers!</h1>
+          <h3 className="text-[1.5rem]">see what are the pricing in details</h3>
+        </div>
+        <div className="flex flex-wrap justify-center items-center w-[90vw] max-w-[80rem] gap-28 ">
+          <ul className="flex flex-wrap justify-center items-center w-[80vw] gap-28 max800:flex-col">
+            {Ctx.productList.map((product) => {
+              return Product(product);
+            })}
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+}
