8446f803ae1b337acbe7ffa148023872fb940c93
mr-soumya071 <soumyaranjansahugcek@gmail.com>
2023-08-27 20:54:28 +0530

    Changes

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -77,7 +77,7 @@ const NavBar = () => {
                   Navigate("/login");
                 }}
               >
-                Login.
+                Login
               </button>
             )}
           </p>
