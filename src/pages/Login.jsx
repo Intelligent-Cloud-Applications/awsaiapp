8781950550b23ce098b674d65a63ca16bd28dee6
Avishek3223 <avishekmishra56@gmail.com>
2023-11-05 12:39:45 +0530

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,28 @@
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Auth, API } from 'aws-amplify';
+import { Auth, API } from "aws-amplify";
 import Context from "../context/Context";
-import Swal from 'sweetalert2';
-import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
-import Navbar from '../components/Home/Navbar';
-import EmailIcon from '../utils/Assets/Dashboard/images/SVG/EmailIcon.svg';
-import LockIcon from '../utils/Assets/Dashboard/images/SVG/LockIcon.svg';
+import Swal from "sweetalert2";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import Navbar from "../components/Home/Navbar";
+import EmailIcon from "../utils/Assets/Dashboard/images/SVG/EmailIcon.svg";
+import LockIcon from "../utils/Assets/Dashboard/images/SVG/LockIcon.svg";
 // import GoogleIcon from '../utils/png/Google.png';
 // import FacebookIcon from '../utils/png/Facebook.png';
-import LoginPng from '../utils/Assets/Login.png';
-import './Login.css';
+import LoginPng from "../utils/Assets/Login.png";
+import "./Login.css";
 
 const Login = () => {
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
   const [passwordVisible, setPasswordVisible] = useState(true);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const UtilCtx = useContext(Context).util;
   const UserCtx = useContext(Context);
+  const institutionName = ["happyprancer", "bworkz", "awsaiapp"];
+  const [institution, setInstitution] = useState("");
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -31,8 +33,9 @@ const Login = () => {
 
   const togglePasswordVisibility = (e) => {
     e.preventDefault();
-    setPasswordVisible(!passwordVisible)
-  }
+    setPasswordVisible(!passwordVisible);
+  };
+
   const handelSubmit = async (event) => {
     event.preventDefault();
 
@@ -42,24 +45,27 @@ const Login = () => {
       const user = await Auth.signIn(formData.email, formData.password);
 
       if (user) {
-        const userdata = await API.get('clients', '/self/read-self/awsaiapp');
-        if (userdata.userType === 'admin' && userdata.institution === "awsaiapp" ) {
+        const userdata = await API.get(
+          "clients",
+          `/self/read-self/${institution}`
+        );
+        if (userdata.userType === "admin") {
           UserCtx.setUserData(userdata);
           UserCtx.setIsAuth(true);
           UtilCtx.setLoader(false);
-          console.log(userdata)
+          console.log(userdata);
           await UserCtx.clients.onReload();
           Swal.fire({
-            icon: 'success',
-            title: 'Welcome Back',
+            icon: "success",
+            title: "Welcome Back",
           });
           Navigate("/dashboard");
         } else {
-          Navigate("/")
+          Navigate("/");
           Swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: 'Please enter a valid Id',
+            icon: "error",
+            title: "Error",
+            text: "Please enter a valid Id",
           });
           UtilCtx.setLoader(false);
         }
@@ -93,14 +99,40 @@ const Login = () => {
       <Navbar />
       <div className=" bg-[#f7f7f7] flex justify-center items-center h-[100vh]">
         <div className="flex max767:flex-col h-[35rem]">
-          <div className=" mobile1 flex flex-col justify-evenly items-center Inter bg-[#30AFBC] p-8 rounded-tl-[2rem] rounded-bl-[2rem] shadow-md w-[30rem] max767:bg-transparent max1050:w-[48vw]" style={{ boxShadow: '0 9px 14px rgba(48, 175, 188, 0.5)' }}>
+          <div
+            className=" mobile1 flex flex-col justify-evenly items-center Inter bg-[#30AFBC] p-8 rounded-tl-[2rem] rounded-bl-[2rem] shadow-md w-[30rem] max767:bg-transparent max1050:w-[48vw]"
+            style={{ boxShadow: "0 9px 14px rgba(48, 175, 188, 0.5)" }}
+          >
             <img src={LoginPng} alt="" />
-            <h1 className='font-[900] text-[2rem] max767:text-[5vw] text-white Laila max767:text-black'>Let’s Get Started</h1>
+            <h1 className="font-[900] text-[2rem] max767:text-[5vw] text-white Laila max767:text-black">
+              Let’s Get Started
+            </h1>
           </div>
 
-          <div className=" mobile2 Inter flex flex-col justify-evenly bg-white p-8 rounded-tr-[2rem] rounded-br-[2rem] shadow-md w-[30rem] max1050:w-[48vw]" style={{ boxShadow: '12px 9px 14px rgba(48, 175, 188, 0.5)' }}>
-            <h2 className="Inter text-center text-2xl font-semibold mb-4">Login</h2>
-            <form className='flex flex-col items-center'>
+          <div
+            className=" mobile2 Inter flex flex-col justify-evenly bg-white p-8 rounded-tr-[2rem] rounded-br-[2rem] shadow-md w-[30rem] max1050:w-[48vw]"
+            style={{ boxShadow: "12px 9px 14px rgba(48, 175, 188, 0.5)" }}
+          >
+            <h2 className="Inter text-center text-2xl font-semibold mb-4">
+              Login
+            </h2>
+            <form className="flex flex-col items-center">
+              <select
+                className="Inter pl-2 w-[20rem] p-2 border rounded-[0.5rem] mb-6"
+                value={institution}
+                onChange={(e) => {
+                  setInstitution(e.target.value);
+                }}
+              >
+                <option className="text-[#474747]" value="">
+                  Select Institution
+                </option>
+                {institutionName.map((name) => (
+                  <option key={name} value={name}>
+                    {name}
+                  </option>
+                ))}
+              </select>
               <div className="mb-4 relative flex items-center">
                 <img
                   src={EmailIcon}
@@ -123,19 +155,25 @@ const Login = () => {
                   className="absolute left-3 top-3 h-[1rem] w-[1rem] pointer-events-none"
                 />
                 <input
-                  type={passwordVisible ? 'password' : 'text'}
+                  type={passwordVisible ? "password" : "text"}
                   name="password"
                   value={formData.password}
                   onChange={handleInputChange}
                   className="Inter pl-10 w-[20rem] p-2 border rounded-[0.5rem]"
                   placeholder="Password"
                 />
-                <button onClick={togglePasswordVisibility} className="absolute right-3 top-3">
+                <button
+                  onClick={togglePasswordVisibility}
+                  className="absolute right-3 top-3"
+                >
                   {passwordVisible ? <AiFillEyeInvisible /> : <AiFillEye />}
                 </button>
               </div>
               <div className="w-[20rem] mb-6">
-                <a href="/forgot-password" className="text-[#017E2B] text-[0.8rem] font-[600] hover:underline">
+                <a
+                  href="/forgot-password"
+                  className="text-[#017E2B] text-[0.8rem] font-[600] hover:underline"
+                >
                   Forgot Password?
                 </a>
               </div>
@@ -151,7 +189,6 @@ const Login = () => {
                   {error}
                 </div>
               )}
-
 
               {/* <div className='flex flex-row items-center justify-center mt-[4rem] mb-[1rem]'>
                 <div className=" w-[6rem] bg-[#000000] h-[0.09rem] mr-1"></div>
@@ -186,7 +223,7 @@ const Login = () => {
         </div>
       </div>
     </>
-  )
+  );
 };
 
 export default Login;
