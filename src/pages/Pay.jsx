d70eb64aff9c0108ba65e3c980986d868fdddc3f
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-12-27 17:36:13 +0530

    Added Pricing of Razorpay

--- src/pages/Pay.jsx
+++ src/pages/Pay.jsx
@@ -3,21 +3,114 @@ import Navbar from "../components/Home/Navbar";
 import { useNavigate } from "react-router-dom";
 import Context from "../context/Context";
 import PayItem from "../components/Pay/PayItem";
+import { API } from "aws-amplify";
 const Pay = () => {
   const [selectedPlan, setSelectedPlan] = useState(null); // To keep track of the selected plan
   const Ctx = useContext(Context);
-  const navigate = useNavigate();
+  const UtilCtx = useContext(Context).util;
+  const UserCtx = useContext(Context).user;
+  const Navigate = useNavigate();
 
   const handleLearnMoreClick = (plan) => {
     setSelectedPlan(plan);
     navigateToPricing();
   };
   const navigateToPricing = () => {
-    navigate("/pricing");
+    Navigate("/pricing");
   };
 
-  const handleApiCall = (plan) => {
-    console.log(plan);
+  const handleSubscribe = async (productId) => {
+    UtilCtx.setLoader(true);
+    let response;
+    try {
+      console.log("before");
+      response = await API.put("clients", "/user/billing/subscription", {
+        body: {
+          productId: productId,
+        },
+      });
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+      UtilCtx.setLoader(false);
+    }
+    console.log(response.paymentId);
+    console.log("started");
+    try {
+      const options = {
+        key: "rzp_test_1nTmB013tmcWZS",
+        subscription_id: response.paymentId,
+        name: "AWSAIAPP",
+        description: response.subscriptionType,
+        handler: function (r) {
+          console.log(r);
+          const verify = async () => {
+            console.log("EARLY");
+            UtilCtx.setLoader(true);
+            try {
+              const res = await API.put(
+                "clients",
+                "/user/billing/subscription/verify",
+                {
+                  body: {
+                    subscriptionId: response.paymentId,
+                  },
+                }
+              );
+              const tempUserdata = await API.get(
+                "clients",
+                "/self/read-self/awsaiapp"
+              );
+              Ctx.setUserData(tempUserdata);
+              console.log(res);
+              if (res.signatureIsValid) {
+                console.log(res.signatureIsValid);
+                Navigate("/dashboard", { state: { isReload: true } });
+              } else {
+                alert(
+                  "Transaction Failed If your Amount was Deducted then Contact us"
+                );
+              }
+              // alert(res);
+              UtilCtx.setLoader(false);
+            } catch (e) {
+              console.log(e);
+              UtilCtx.setLoader(false);
+            }
+          };
+          verify();
+        },
+        prefill: {
+          name: UserCtx.userName,
+          email: UserCtx.emailId,
+          contact: UserCtx.phoneNumber,
+        },
+        theme: {
+          color: "#00b4bb",
+        },
+      };
+      console.log("started 2");
+      const rzp1 = new window.Razorpay(options);
+      console.log("started 3");
+      rzp1.on("payment.failed", function (response) {
+        // alert(response.error.code);
+        // alert(response.error.description);
+        // alert(response.error.source);
+        // alert(response.error.step);
+        // alert(response.error.reason);
+        // alert(response.error.metadata.order_id);
+        // alert(response.error.metadata.payment_id);
+        console.log(response);
+        UtilCtx.setLoader(false);
+      });
+      const fields = rzp1.open();
+      console.log(fields);
+      UtilCtx.setLoader(false);
+    } catch (e) {
+      console.log(e.message);
+      console.log(e);
+      UtilCtx.setLoader(false);
+    }
   };
 
   return (
@@ -42,7 +135,7 @@ const Pay = () => {
       {selectedPlan && (
         <div className="flex justify-center mt-6">
           <button
-            onClick={() => handleApiCall(selectedPlan)}
+            onClick={() => handleSubscribe(selectedPlan)}
             className="bg-[#30AFBC] hover:bg-slate-900 text-white py-2 px-6 rounded duration-300 hover:scale-105 shadow-lg"
           >
             Pay
--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -5,73 +5,162 @@ import Footer from "../components/Home/Footer";
 import { Box } from "@mui/system";
 import { useNavigate } from "react-router-dom";
 import Context from "../context/Context";
+import { API } from "aws-amplify";
 
 const Pricing = () => {
-    const Navigate = useNavigate();
-    const UserCtx = useContext(Context);
-    
-    const handleGetStartedClick = () => {
-        if (UserCtx.isAuth) {
-            Navigate('/subpopup');
-        } else {
-            Navigate('/signup');
-        }
-    };
-    const handleGetStartedClick1 = () => {
-        if (UserCtx.isAuth) {
-            Navigate('/subpopup1');
-        } else {
-            Navigate('/signup');
-        }
-    };
-    const handleGetStartedClick2 = () => {
-        if (UserCtx.isAuth) {
-            Navigate('/subpopup2');
-        } else {
-            Navigate('/signup');
-        }
-    };
-    return (
-        <div className="flex flex-col">
-            <Navbar />
-            <div className=" background flex flex-col items-center w-full pb-[5rem]">
-                <div className="gap-6 p-20 pt-40 max-w-5xl mx-auto text-center head max600:pt-20">
-                    <h1 className="font-bold text-center justify-center text-15xl md:text-10xl lg:text-15xl xl:text-18xl text-black max767:text-white ">
-                        Find the plan that is right <span className="text-white">for you</span>
-                    </h1>
-                </div>
+  const Navigate = useNavigate();
+  const UserCtx = useContext(Context);
+  const Ctx = useContext(Context);
+  const UtilCtx = useContext(Context).util;
 
-                <div className="flex flex-col md:flex-row justify-center items-center gap-5 pb-10 mx-auto pos xl:gap-10">
-                    <Box sx={{
-                        height: {
-                            sm: "48rem",
-                            md: "45rem",
-                            lg: "41rem",
-                        },
-                    }} className="box flex flex-col bg-white xl:w-[25rem] rounded-lg shadow-lg">
-                        <h3 className="font-semibold text-2xl xl:text-3xl pb-2">Basic</h3>
-                        <p className="text-2xl xl:text-3xl font-semibold">₹ 1,000</p>
-                        <p className="text-sm xl:text-base pb-4">Per Month</p>
-                        <h4 className="text-lg xl:text-xl font-semibold pb-2">Features</h4>
-                        <ul className="text-sm p-2">
-                            <li>Establish a strong online presence.</li>
-                            <li>Stunning static website design.</li>
-                            <li>User-responsive and user-friendly interface.</li>
-                            <li>Web hosting included.</li>
-                            <li>Regular website maintenance.</li>
-                            <li>Fast and easy page loading.</li>
-                            <li>SSL certificate for secure data transmission</li>
-                            <li>Monthly billing for flexibility.</li>
-                            <li>Customization options available.</li>
-                        </ul>
-                        <button
-                            onClick={handleGetStartedClick}
-                            className="text-white text-lg xl:text-xl font-semibold bg-black hover:bg-[#30AFBC] hover:text-black py-2 px-4 rounded-lg">
-                            Get Started
-                        </button>
-                    </Box>
-                    {/* Middle Box */}
-                    <Box sx={{
+  const handleSubscribe = async (productId) => {
+    UtilCtx.setLoader(true);
+    let response;
+    try {
+      console.log("before");
+      response = await API.put("clients", "/user/billing/subscription", {
+        body: {
+          productId: productId,
+        },
+      });
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+      UtilCtx.setLoader(false);
+    }
+    console.log(response.paymentId);
+    console.log("started");
+    try {
+      const options = {
+        key: "rzp_test_1nTmB013tmcWZS",
+        subscription_id: response.paymentId,
+        name: "AWSAIAPP",
+        description: response.subscriptionType,
+        handler: function (r) {
+          console.log(r);
+          const verify = async () => {
+            console.log("EARLY");
+            UtilCtx.setLoader(true);
+            try {
+              const res = await API.put(
+                "clients",
+                "/user/billing/subscription/verify",
+                {
+                  body: {
+                    subscriptionId: response.paymentId,
+                  },
+                }
+              );
+              const tempUserdata = await API.get(
+                "clients",
+                "/self/read-self/awsaiapp"
+              );
+              Ctx.setUserData(tempUserdata);
+              console.log(res);
+              if (res.signatureIsValid) {
+                console.log(res.signatureIsValid);
+                Navigate("/dashboard", { state: { isReload: true } });
+              } else {
+                alert(
+                  "Transaction Failed If your Amount was Deducted then Contact us"
+                );
+              }
+              // alert(res);
+              UtilCtx.setLoader(false);
+            } catch (e) {
+              console.log(e);
+              UtilCtx.setLoader(false);
+            }
+          };
+          verify();
+        },
+        prefill: {
+          name: UserCtx.userName,
+          email: UserCtx.emailId,
+          contact: UserCtx.phoneNumber,
+        },
+        theme: {
+          color: "#00b4bb",
+        },
+      };
+      console.log("started 2");
+      const rzp1 = new window.Razorpay(options);
+      console.log("started 3");
+      rzp1.on("payment.failed", function (response) {
+        // alert(response.error.code);
+        // alert(response.error.description);
+        // alert(response.error.source);
+        // alert(response.error.step);
+        // alert(response.error.reason);
+        // alert(response.error.metadata.order_id);
+        // alert(response.error.metadata.payment_id);
+        console.log(response);
+        UtilCtx.setLoader(false);
+      });
+      const fields = rzp1.open();
+      console.log(fields);
+      UtilCtx.setLoader(false);
+    } catch (e) {
+      console.log(e.message);
+      console.log(e);
+      UtilCtx.setLoader(false);
+    }
+  };
+
+  return (
+    <div className="flex flex-col">
+      <Navbar />
+      <div className=" background flex flex-col items-center w-full pb-[5rem]">
+        <div className="gap-6 p-20 pt-40 max-w-5xl mx-auto text-center head max600:pt-20">
+          <h1 className="font-bold text-center justify-center text-15xl md:text-10xl lg:text-15xl xl:text-18xl text-black max767:text-white ">
+            Find the plan that is right{" "}
+            <span className="text-white">for you</span>
+          </h1>
+        </div>
+
+        <div className="flex flex-col md:flex-row justify-center items-center gap-5 pb-10 mx-auto pos xl:gap-10">
+          {Ctx.products.map((product) => {
+            return (
+              <Box
+                key={product.productId}
+                sx={{
+                  height: {
+                    sm: "48rem",
+                    md: "45rem",
+                    lg: "52rem",
+                  },
+                }}
+                className="box flex flex-col bg-white xl:w-[25rem] rounded-lg shadow-lg"
+              >
+                <h3 className="font-semibold text-2xl xl:text-3xl pb-2">
+                  {product.heading}
+                </h3>
+                <p className="text-2xl xl:text-3xl font-semibold">
+                  ₹ {product.amount / 100}
+                </p>
+                <p className="text-sm xl:text-base pb-4">
+                  Per {product.durationText}
+                </p>
+                <h4 className="text-lg xl:text-xl font-semibold pb-2">
+                  Features
+                </h4>
+                <ul className="text-sm p-2">
+                  {product.provides.map((feature) => {
+                    return <li>{feature}</li>;
+                  })}
+                </ul>
+                <button
+                  onClick={() => {
+                    handleSubscribe(product.productId.toString());
+                  }}
+                  className="text-white text-lg xl:text-xl font-semibold bg-black hover:bg-[#30AFBC] hover:text-black py-2 px-4 rounded-lg"
+                >
+                  Get Started
+                </button>
+              </Box>
+            );
+          })}
+          {/* <Box sx={{
                         height: {
                             sm: "54rem",
                             md: "50rem",
@@ -139,12 +228,12 @@ const Pricing = () => {
                             Get Started
                         </button>
 
-                    </Box>
-                </div>
-            </div>
-            <Footer />
+                    </Box> */}
         </div>
-    );
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Pricing;
