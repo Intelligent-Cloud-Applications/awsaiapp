b39aeed87d03c9bc47591c293c22d0a640c85d5c
ShreetamMishra <shreetammishra1@gmail.com>
2024-08-18 23:05:20 +0530

    un used variable solve

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-// import Login from "./pages/Login";
+import Login from "./pages/Login";
 import Logout from "./pages/Auth/Logout";
 import Auth from "./pages/Auth";
 import DashBoard from "./pages/DashBoard";
@@ -20,7 +20,7 @@ import Customer from "./services/Customer";
 import Payment from "./services/Payment";
 import Terms from "./pages/Terms";
 import Refund from "./pages/Refund";
-// import SignUp from "./pages/SignUp";
+import SignUp from "./pages/SignUp";
 import SubscriptionPopup from "./pages/Subscribe_POPUP";
 import SubscriptionPopup1 from "./pages/Subscribe_POPUP1";
 import SubscriptionPopup2 from "./pages/Subscribe_POPUP2";
@@ -53,8 +53,8 @@ const RoutesContainer = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} /> */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/subpopup" element={<SubscriptionPopup />} />
       <Route path="/subpopup1" element={<SubscriptionPopup1 />} />
       <Route path="/subpopup2" element={<SubscriptionPopup2 />} />
