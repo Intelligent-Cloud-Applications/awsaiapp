a86e9e6bc94ca1e246bfd38f811ce885e4ca8529
Infinity2209 <anantanag91@gmail.com>
2024-08-31 10:11:57 +0530

    removing the warning

--- src/components/Dashboard/MemberList/Index.jsx
+++ src/components/Dashboard/MemberList/Index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import NewMemberList from './NewMemberList';
 import ButtonGroup from '../../../Common/DashboardNav/ButtonGroup';
 import Navbar from '../../Home/Navbar';
