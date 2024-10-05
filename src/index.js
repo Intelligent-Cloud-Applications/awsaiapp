8817cbedb087a617e92aa30d70d1a9d3e9d99307
ShreetamMishra <shreetammishra1@gmail.com>
2024-10-05 22:59:27 +0530

    temporary login for test

--- src/index.js
+++ src/index.js
@@ -16,21 +16,24 @@ Amplify.configure({
   Auth: {
     mandatorySignIn: true,
     region: process.env.REACT_APP_STAGE === "PROD" ? "us-east-1": "us-east-2",
-    userPoolId:
-      process.env.REACT_APP_STAGE === "PROD"
-        ? "us-east-1_a3Fk5S3hh"
-        : "us-east-2_L3E5BSjIf",
-    identityPoolId:
-      process.env.REACT_APP_STAGE === "PROD"
-        ? "us-east-1:a68cac30-d7f7-4f73-9b1f-ca6a4f86eba6"
-        : "us-east-2:9b1fda39-3231-4606-b32f-7ba24edcb53d",
-    userPoolWebClientId:
-      process.env.REACT_APP_STAGE === "PROD"
-        ? "jevucp6v2ehehqerq0rlgn4d8"
-        : "5pqia04a65b3ef6fbmpoccj4vl",
-    oauth: {
-      responseType: "token",
-    },
+    // userPoolId:
+    //   process.env.REACT_APP_STAGE === "PROD"
+    //     ? "us-east-1_a3Fk5S3hh"
+    //     : "us-east-2_L3E5BSjIf",
+    // identityPoolId:
+    //   process.env.REACT_APP_STAGE === "PROD"
+    //     ? "us-east-1:a68cac30-d7f7-4f73-9b1f-ca6a4f86eba6"
+    //     : "us-east-2:9b1fda39-3231-4606-b32f-7ba24edcb53d",
+    // userPoolWebClientId:
+    //   process.env.REACT_APP_STAGE === "PROD"
+    //     ? "jevucp6v2ehehqerq0rlgn4d8"
+    //     : "5pqia04a65b3ef6fbmpoccj4vl",
+    // oauth: {
+    //   responseType: "token",
+    // },
+    userPoolId: 'us-east-2_J02pfxenV',
+    identityPoolId: 'us-east-2:2966c931-c163-4682-89d6-9bf8c491e5b7',
+    userPoolWebClientId: '1oui8eijud46ajipjeg01h4i3m',
   },
   Storage: {
     region: "us-east-1",
