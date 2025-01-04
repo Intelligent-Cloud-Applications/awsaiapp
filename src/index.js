83223a71c4198ce7000503db96692397200e7a98
Infinity2209 <anantanag91@gmail.com>
2025-01-04 12:54:34 +0530

    beta for sit

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ Amplify.configure({
     region: "us-east-1",
     bucket: "institution-utils",
     identityPoolId:
-      process.env.STAGE === "PROD"
+      process.env.REACT_APP_STAGE === "PROD"
         ? "us-east-1:a68cac30-d7f7-4f73-9b1f-ca6a4f86eba6"
         : "us-east-1:a68cac30-d7f7-4f73-9b1f-ca6a4f86eba6",
     additionalIdentityPoolId: "us-east-2:9b1fda39-3231-4606-b32f-7ba24edcb53d"
@@ -43,10 +43,10 @@ Amplify.configure({
       {
         name: "clients",
         endpoint:
-          process.env.STAGE === "PROD"
-            ? "https://lo0fd7fboa.execute-api.us-east-1.amazonaws.com/prod"
+          process.env.REACT_APP_STAGE === "PROD"
+            ? "https://er9zh7i7md.execute-api.us-east-1.amazonaws.com/dev"
             : "https://gn41h453j1.execute-api.us-east-2.amazonaws.com/dev",
-        region: process.env.STAGE === "PROD" ? "us-east-1" : "us-east-2",
+        region: process.env.REACT_APP_STAGE === "PROD" ? "us-east-1" : "us-east-2",
       },
       {
         name: 'beta_dance',
