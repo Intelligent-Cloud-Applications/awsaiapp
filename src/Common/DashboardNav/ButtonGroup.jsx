47f7c33fe3b3987d140b170b5d4969c24a28cf77
Infinity2209 <anantanag91@gmail.com>
2025-01-10 16:14:42 +0530

    the dashboard is enhanced

--- src/Common/DashboardNav/ButtonGroup.jsx
+++ src/Common/DashboardNav/ButtonGroup.jsx
@@ -5,12 +5,9 @@ import { HiUserGroup, HiUserAdd } from 'react-icons/hi';
 import { MdEdit } from 'react-icons/md';
 import { useNavigate } from "react-router-dom";
 
-const ButtonGroup = ({ onTabChange,institutionNames }) => {
+const ButtonGroup = ({ onTabChange, institutionNames, institutionType }) => {
   const [activeTab, setActiveTab] = useState('members');
   const navigate = useNavigate();
-  // const location = useLocation();
-  // const queryParams = new URLSearchParams(location.search);
-  // const institutionNames = queryParams.get("institution");
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
@@ -18,16 +15,16 @@ const ButtonGroup = ({ onTabChange,institutionNames }) => {
   };
 
   return (
-    <div className="flex items-center justify-start"> {/* Aligns items horizontally */}
+    <div className="flex items-center justify-start">
       <Button.Group className="flex h-12">
         <Button
           style={{
             backgroundColor: activeTab === 'members' ? '#30afbc' : '#fff',
             color: activeTab === 'members' ? '#fff' : '#000',
-            borderColor: activeTab === 'members' ? '#30afbc' : '#ccc', 
+            borderColor: activeTab === 'members' ? '#30afbc' : '#ccc',
           }}
           onClick={() => handleTabChange('members')}
-          className={`items-center hover:bg-[#30afbc] hover:text-white border rounded-l-md`}
+          className="items-center hover:bg-[#30afbc] hover:text-white border rounded-l-md"
         >
           <HiUserGroup className="mr-3 h-4 w-4" />
           Members List
@@ -36,10 +33,10 @@ const ButtonGroup = ({ onTabChange,institutionNames }) => {
           style={{
             backgroundColor: activeTab === 'economy' ? '#30afbc' : '#fff',
             color: activeTab === 'economy' ? '#fff' : '#000',
-            borderColor: activeTab === 'economy' ? '#30afbc' : '#ccc', 
+            borderColor: activeTab === 'economy' ? '#30afbc' : '#ccc',
           }}
           onClick={() => handleTabChange('economy')}
-          className={`items-center hover:bg-[#30afbc] hover:text-white border`}
+          className="items-center hover:bg-[#30afbc] hover:text-white border"
         >
           <IoMdCash className="mr-3 h-4 w-4 mt-0.5" />
           Economy
@@ -48,10 +45,10 @@ const ButtonGroup = ({ onTabChange,institutionNames }) => {
           style={{
             backgroundColor: activeTab === 'leads' ? '#30afbc' : '#fff',
             color: activeTab === 'leads' ? '#fff' : '#000',
-            borderColor: activeTab === 'leads' ? '#30afbc' : '#ccc', 
+            borderColor: activeTab === 'leads' ? '#30afbc' : '#ccc',
           }}
           onClick={() => handleTabChange('leads')}
-          className={`items-center hover:bg-[#30afbc] hover:text-white border`}
+          className="items-center hover:bg-[#30afbc] hover:text-white border"
         >
           <HiUserAdd className="mr-3 h-4 w-4" />
           Leads
@@ -60,25 +57,26 @@ const ButtonGroup = ({ onTabChange,institutionNames }) => {
           style={{
             backgroundColor: activeTab === 'client' ? '#30afbc' : '#fff',
             color: activeTab === 'client' ? '#fff' : '#000',
-            borderColor: activeTab === 'client' ? '#30afbc' : '#ccc', 
+            borderColor: activeTab === 'client' ? '#30afbc' : '#ccc',
           }}
           onClick={() => handleTabChange('client')}
-          className={`rounded-r-md items-center hover:bg-[#30afbc] hover:text-white border`}
+          className="rounded-r-md items-center hover:bg-[#30afbc] hover:text-white border"
         >
           <IoMdPerson className="mr-3 h-4 w-4" />
           Profile
         </Button>
       </Button.Group>
-      
+
       <Button
         style={{
           backgroundColor: '#30afbc',
           color: '#fff',
         }}
-        className="flex items-center rounded-md ml-[38rem]"  // Adjust the margin-left (ml-4) for desired spacing
+        className="flex items-center rounded-md ml-[38rem]"
         onClick={() => {
+          const targetPath = institutionType === 'dancestudio' ? '/full' : '/completeDraft';
           navigate({
-            pathname: '/full',
+            pathname: targetPath,
             search: `?institutionName=${institutionNames}`,
           });
         }}
