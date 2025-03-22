0a69436944574be91144c106f591703559e34fe3
Infinity2209 <anantanag91@gmail.com>
2025-03-22 17:09:35 +0530

    responsiveness is fixed

--- src/components/Dashboard/Panel/StatsGrid.jsx
+++ src/components/Dashboard/Panel/StatsGrid.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import PropTypes from 'prop-types';
 import { FiLoader, FiCheckCircle, FiClock, FiTrendingUp } from 'react-icons/fi';
 import { STATS_CONFIG } from './Constant';
@@ -10,7 +11,9 @@ const IconMap = {
     FiTrendingUp
 };
 
-const StatsGrid = ({ stats, onStatClick, activeSort }) => {
+const StatsGrid = ({ stats, onStatClick }) => {
+    const [activeIndex, setActiveIndex] = useState(null);
+
 
     const renderIcon = (iconName) => {
         const Icon = IconMap[iconName];
@@ -28,12 +31,14 @@ const filteredStats = {
         <div className="grid grid-cols-2 gap-10 mb-3 [@media(min-width:1023px)]:grid-cols-3">
             {STATS_CONFIG.map((config, index) => {
                 const statValue = filteredStats[config.id] || 0;
-
                 return (
                     <div
                         key={config.id}
-                        onClick={() => onStatClick(config.sortConfig, index)}
-                        className={`bg-white rounded-lg shadow-md hover:shadow-lg transition-all duration-200 p-4 cursor-pointer ${activeSort === index ? 'ring-1 ring-cyan-500 shadow-md' : ''
+                        onClick={() => {
+                            setActiveIndex(activeIndex === index ? null : index);
+                            onStatClick(config.sortConfig, index);
+                        }}
+                        className={`bg-white rounded-lg shadow-md hover:shadow-lg transition-all duration-200 p-4 cursor-pointer ${activeIndex === index ? 'ring-1 ring-cyan-500 shadow-md' : ''
                             }`}
                     >
                         <div className="flex justify-between items-start gap-4">
